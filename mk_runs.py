76620825db967d78c34a3be7fe4d36f081965176
Peter Teuben <teuben@gmail.com>
2024-11-29 12:16:37 +0000

    new data

--- mk_runs.py
+++ mk_runs.py
@@ -11,7 +11,8 @@ project="2024-S1-MX-24"
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
 on['Per-emb-2'] = [113271, 113408, 113410, 113412,
-                   123258, 123260, 123262, 123319, 123321, 123323, 123327,]     # nov 25/26
+                   123258, 123260, 123262, 123319, 123321, 123323, 123327,     # nov 25/26
+                   123493, 123495, 123497, 123503, 123505, 123507,]            # nov 27
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
@@ -20,7 +21,7 @@ pars1['Per-emb-2']   = "dv=5 dw=20 b_order=1"
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
-pars2['Per-emb-2']   = "srdp=1 admit=0"
+pars2['Per-emb-2']   = "srdp=1 admit=0 pix_list=-3,14"
 
 if __name__ == '__main__':    
     runs.mk_runs(project, on, pars1, pars2, None, sys.argv)
